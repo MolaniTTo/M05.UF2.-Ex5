77b915376637024b18c681729e9edf660a78a4fb
MolaniTTo <arnau.molano.7e7@itb.cat>
2024-01-08 12:30:34 +0000

    Add project files.

---
+++ Menu/Program.cs
@@ -0,0 +1,51 @@
+﻿using Metodes;
+
+namespace M03.UF2.Ex2
+{
+    public class Ex2Project
+    {
+        public static void Main()
+        {
+            const string Validar = "1-Si vols validar un nombre senar";
+            const string Potencia = "2-Si vols Calcular la potencia d'un nombre";
+            const string Random = "3-Si vols un valor aleatori";
+            const string ConsVocal = "4-Si vols comptar el nombre de vocals i consonants d'un text";
+            const string Sortir = "5-Si vols sortir";
+
+            Console.WriteLine(Validar);
+            Console.WriteLine(Potencia);
+            Console.WriteLine(Random);
+            Console.WriteLine(ConsVocal);
+            Console.WriteLine(Sortir);
+
+
+
+            int funcionalitat;
+            funcionalitat = Convert.ToInt32(Console.ReadLine());
+            switch (funcionalitat)
+            {
+                case 1:
+                    Console.WriteLine(Class1.Validacio());
+                    break;
+
+                case 2:
+                    Console.WriteLine(Class1.Potencia());
+                    break;
+                case 3:
+                    Console.WriteLine(Class1.Random());
+                    break;
+
+                case 4:
+                    Console.WriteLine(Class1.Contador());
+                    break;
+                case 5:
+                    Console.WriteLine(Class1.Adeu());
+                    break;
+
+
+            }
+
+        }
+
+    }
+}
---
+++ Metodes/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Metodes
+{
+    public class Class1
+    {
+        public static bool Validacio()
+        {
+            int num;
+            Console.WriteLine("Introdueix un numero");
+            num =Convert.ToInt32(Console.ReadLine());
+              
+            if (num%2==0) {return false;} 
+            else { return true;}
+        }
+        public static int Potencia() 
+        {
+            int num, exp;
+            Console.WriteLine("Introdueix un numero");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introdueix l'exponent");
+            exp = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < exp; i++)
+            {
+                num = num * num;
+            }
+            return num;
+
+        }
+        public static string Random()
+        {
+            string s = "No l'he fet";
+            return s;
+
+        }
+
+
+        public static string Contador()
+        {
+            string Text = "";
+            int vocals = 0, consonants=0;
+            
+            Text = Console.ReadLine();
+            foreach (char c in Text.ToCharArray() ) 
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                {
+                    vocals++;
+                }
+                else { consonants++; }
+
+            }
+            string retorn = $"El nombre de vocals és {vocals} i el de consonants és {consonants}";
+            return retorn;
+
+        }
+
+
+        public static string Adeu()
+        {
+            const string MsgAdeu = "Fins aviat!";
+            return MsgAdeu; 
+
+        }
+
+
+    }
+}
